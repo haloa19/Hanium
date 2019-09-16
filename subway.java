751b7f066180af0270951b226b0961d957f8438c
haloa19 <52286744+haloa19@users.noreply.github.com>
2019-09-17 00:43:04 +0900

    Hanium 최종코드

---
+++ subway.java
@@ -0,0 +1,43 @@
+package yalantis.com.sidemenu.sample;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.mospi.moml.framework.pub.core.MOMLFragmentActivity;
+
+/**
+ * Created by jenny on 2016-08-31.
+ */
+public class subway  extends MOMLFragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        loadApplication("moml/applicationInfo.xml");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
